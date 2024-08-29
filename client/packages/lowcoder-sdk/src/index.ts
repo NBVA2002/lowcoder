f4be6718b682ccf244a54a5f0a393e4841bb3184
NBVA2002 <117577336+NBVA2002@users.noreply.github.com>
2024-08-29 07:56:46 +0000

    commit

--- client/packages/lowcoder-sdk/src/index.ts
+++ client/packages/lowcoder-sdk/src/index.ts
@@ -3,8 +3,8 @@ import "../../lowcoder/src/index.less";
 import "virtual:globals";
 import * as sdk from "../../lowcoder";
 export * from "../../lowcoder";
-import { bootstrap } from "../../lowcoder/app";
+import { bootstrap } from "../../lowcoder/src/app";
 
-export bootstrap;
+export { bootstrap };
 
 window.$lowcoder_sdk = sdk;
